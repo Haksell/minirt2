54f35188591433c84a0240af4ad9a61f5dc26cbb
Axel Brisse <axel.brisse@gmail.com>
2023-09-22 08:22:13 +0200

    rm vec3_dist unused in old project

--- minirt.h
+++ minirt.h
@@ -142,6 +142,5 @@ float	vec3_length_squared(t_vec3 v);
 float	vec3_length(t_vec3 v);
 t_vec3	vec3_unit(t_vec3 v);
 float	vec3_dist_squared(t_vec3 v1, t_vec3 v2);
-float	vec3_dist(t_vec3 v1, t_vec3 v2);
 
 #endif
--- srcs/vec3/vec3b.c
+++ srcs/vec3/vec3b.c
@@ -19,8 +19,3 @@ float	vec3_dist_squared(t_vec3 v1, t_vec3 v2)
 {
 	return (vec3_length_squared(v1 - v2));
 }
-
-float	vec3_dist(t_vec3 v1, t_vec3 v2)
-{
-	return (vec3_length(v1 - v2));
-}
