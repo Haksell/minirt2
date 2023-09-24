e625efc06f8fe82a60b0d97934674805011a1a70
Axel Brisse <axel.brisse@gmail.com>
2023-09-24 14:21:14 +0200

    one-liner hit_disk I'm so leet

--- srcs/hit/hit_disk.c
+++ srcs/hit/hit_disk.c
@@ -3,10 +3,7 @@
 bool	hit_disk(t_hit *hit, const t_disk *disk, t_ray *ray,
 	t_interval interval)
 {
-	float	dist_squared;
-
-	if (!hit_plane(hit, &disk->plane, ray, interval))
-		return (false);
-	dist_squared = vec3_dist_squared(disk->plane.coord, hit->point);
-	return (dist_squared <= disk->radius * disk->radius);
+	return (hit_plane(hit, &disk->plane, ray, interval)
+		&& vec3_dist_squared(disk->plane.coord, hit->point)
+		<= disk->radius * disk->radius);
 }
