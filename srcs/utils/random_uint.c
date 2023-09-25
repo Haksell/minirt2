4bb9d03c39fb49eb6ce53837624b1366227df97e
Axel Brisse <axel.brisse@gmail.com>
2023-09-25 17:56:06 +0200

    remove mutex on random_uint for now

--- srcs/utils/random_uint.c
+++ srcs/utils/random_uint.c
@@ -3,13 +3,13 @@
 unsigned int	get_random_uint(void)
 {
 	static unsigned int		x = UINT_SEED;
-	static pthread_mutex_t	mutex = PTHREAD_MUTEX_INITIALIZER;
+	// static pthread_mutex_t	mutex = PTHREAD_MUTEX_INITIALIZER; //TO DO: check if we can improve
 
-	pthread_mutex_lock(&mutex);
+	// pthread_mutex_lock(&mutex);
 	x ^= x << 13;
 	x ^= x >> 17;
 	x ^= x << 5;
-	pthread_mutex_unlock(&mutex);
+	// pthread_mutex_unlock(&mutex);
 	return (x);
 }
 
