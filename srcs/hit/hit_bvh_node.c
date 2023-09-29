2db4d537a8bb2e258aee596b12b66308c40b88c9
Axel Brisse <axel.brisse@gmail.com>
2023-09-29 13:06:10 +0200

    hit_bvh_node wip

---
+++ srcs/hit/hit_bvh_node.c
@@ -0,0 +1,37 @@
+#include "minirt.h"
+
+static void	swap_floats(float *a, float *b)
+{
+	float	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+bool	hit_bvh_node(t_hit *hit, t_bvh_node *bvh_node, t_ray *ray, t_interval interval)
+{
+	t_interval	axis;
+	size_t		i;
+	float		inv_d;
+	float		t0;
+	float		t1;
+
+	(void)hit; // TODO
+	i = 0;
+	while (i < 3)
+	{
+		axis = ((t_interval *)bbox)[i];
+		inv_d = 1.0 / ray->direction[i];
+		t0 = (axis.min - ray->origin[i]) * inv_d;
+		t1 = (axis.max - ray->origin[i]) * inv_d;
+		if (inv_d < 0)
+			swap_floats(&t0, &t1);
+		interval.min = fmaxf(t0, interval.min);
+		interval.max = fminf(t1, interval.max);
+		if (interval.max <= interval.min)
+			return (false);
+		++i;
+	}
+	return (true);
+}
